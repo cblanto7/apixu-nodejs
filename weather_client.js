2b915730439ddb0a1c42a31bedcd8bbb7817ba06
Casey Blanton <crb1987@gmail.com>
2017-08-31 18:15:02 -0600

    GET request working

--- weather_client.js
+++ weather_client.js
@@ -1,11 +1,43 @@
-var weather = require('./weatherlib');
+var weather = require('./weatherlib')
 
-errorHandler = function (){
-	console.log('got some error')
+const NUM_DAYS_REQ = 7
+
+var errorHandler = function () {
+  console.log('got some error')
 }
-	
-//current weather takes pin code or location as first parameter, error handler callback as second
-weather.currentWeather(20500, errorHandler);
 
-//forecast weather takes pin code or location as first parameter, number of days as second, error handler callback as second
-weather.forecastWeather(20500, 2, errorHandler);
+var parseObject = function (obj) {
+  console.log('Parsed some JSON Object')
+  var days = []
+
+  // fill days array
+  var fLen = obj.forecast.forecastday.length
+  for (var i = 0; i < fLen; i++) {
+    days[i] = obj.forecast.forecastday[i]
+  }
+
+  // output date of each day
+  var dLen = days.length
+  for (i = 0; i < dLen; i++) {
+    console.log(days[i].date)
+    for (var j = 0; j < days[i].hour.length; j++) {
+      console.log('\t' + days[i].hour[j].time)
+      console.log('\t\t Temperature(F) :' + days[i].hour[j].temp_f)
+      console.log('\t\t Night(0)-Day(1):' + days[i].hour[j].is_day)
+      console.log('\t\t Wind Speed(Mph):' + days[i].hour[j].wind_mph)
+      console.log('\t\t Wind Direction :' + days[i].hour[j].wind_dir)
+      console.log('\t\t Conditions     :' + days[i].hour[j].condition.text)
+    }
+  }
+
+  /* same thing
+  days.forEach(function (value, index, fullArray) {
+    console.log(value.date + ' is index #: ' + index)
+  }) */
+}
+
+// current weather takes pin code or location as first parameter, error handler callback as second
+// weather.currentWeather(20500, parseObject, errorHandler);
+
+// forecast weather takes pin code or location as first parameter, number of days as second, error handler callback as second
+weather.forecastWeather(20500, errorHandler, NUM_DAYS_REQ, parseObject)
--- weatherlib.js
+++ weatherlib.js
@@ -1,48 +1,48 @@
-http = require('http');
+var http = require('http')
 
-var apiKey = 'cacdf29dc2be47d484a105606152306'
+var apiKey = '47a66ed4fe02423fafc212559172208'
 
 var options = {
   host: 'api.apixu.com',
   port: 80,
   path: '/v1/current.json?key=' + apiKey + '&q=',
   method: 'GET'
-};
-
-
-
-exports.currentWeather = function currentWeather(query, callback){
-	options.path = '/v1/current.json?key=' + apiKey + '&q=' + query;
-	http.request(options, function(res) {
-	  res.setEncoding('utf8');
-	  res.on('data', function (chunk) {
-		console.log(chunk);
-	  });
-	  res.on('end', function (chunk) {
-	  });
-	}).on('error', function(err) {
-        // handle errors with the request itself
-        console.error('Error with the request:', err.message);
-        callback(err);
-    }).end();
 }
 
-
-
-
-
-exports.forecastWeather = function forecastWeather(query, noOfDays, callback){
-	options.path = '/v1/forecast.json?key=' + apiKey + '&q=' + query + '&days=' + noOfDays;
-	http.request(options, function(res) {
-	  res.setEncoding('utf8');
-	  res.on('data', function (chunk) {
-		console.log(chunk);
-	  });
-	  res.on('end', function (chunk) {
-	  });
-	}).on('error', function(err) {
+exports.currentWeather = function currentWeather (query, callback, errorcallback) {
+  options.path = '/v1/current.json?key=' + apiKey + '&q=' + query
+  http.request(options, function (res) {
+    res.setEncoding('utf8')
+    var body = ''
+    res.on('data', function (chunk) {
+      body += chunk
+    })
+    res.on('end', function (chunk) {
+      var obj = JSON.parse(body)
+      callback(obj)
+    })
+  }).on('error', function (err) {
         // handle errors with the request itself
-        console.error('Error with the request:', err.message);
-        callback(err);
-    }).end();
+    console.error('Error with the request:', err.message)
+    errorcallback(err)
+  }).end()
+}
+
+exports.forecastWeather = function forecastWeather (query, errorcallback, noOfDays, callback) {
+  options.path = '/v1/forecast.json?key=' + apiKey + '&q=' + query + '&days=' + noOfDays
+  http.request(options, function (res) {
+    res.setEncoding('utf8')
+    var body = ''
+    res.on('data', function (chunk) {
+      body += chunk
+    })
+    res.on('end', function (chunk) {
+      var obj = JSON.parse(body)
+      callback(obj)
+    })
+  }).on('error', function (err) {
+        // handle errors with the request itself
+    console.error('Error with the request:', err.message)
+    errorcallback(err)
+  }).end()
 }
